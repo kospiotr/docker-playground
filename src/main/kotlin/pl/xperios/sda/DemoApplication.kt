6b28710381e77034e4af4b9e9b214e810f315457
Piotr Kosmowski <kospiotr@tlen.pl>
2017-06-10 21:49:16 +0200

    Update

---
+++ src/main/kotlin/pl/xperios/sda/DemoApplication.kt
@@ -0,0 +1,11 @@
+package pl.xperios.sda
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class DemoApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(DemoApplication::class.java, *args)
+}
---
+++ src/main/kotlin/pl/xperios/sda/SampleRest.kt
@@ -0,0 +1,21 @@
+package pl.xperios.sda
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class SampleRest {
+
+
+    @GetMapping("/hello")
+    fun hello(): String {
+        return "hello"
+    }
+
+    @RequestMapping("/world")
+    fun world(): String {
+        return "world"
+    }
+
+}
